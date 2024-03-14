c58ca2b70860bdf403aa52f7981a9bf4c0ef6973
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-03-14 18:15:15 +0100

    :art: start with python  :art:

--- helloworld.py
+++ helloworld.py
@@ -16,3 +16,46 @@ print(type([1, 2, 3]))   # List
 print(type({'name':'Asabeneh'})) # Dictionary
 print(type({9.8, 3.14, 2.7}))    # Set
 print(type((9.8, 3.14, 2.7)))    # Tuple
+
+
+#Exercise: Level 1
+
+print(3 + 4);
+print(3 - 4);
+print(3 * 4);
+print(3 % 4);
+print(3 / 4);
+print(3 ** 4);
+print(3 // 4);
+
+
+print(type(10));
+print(type(9.8));
+print(type(3.14));
+print(type(4-4j));
+print(type(["lolo", "lea"]));
+print(type("ali"));
+print(type("saldman"));
+print(type("France"));
+
+# Exercise: Level 3
+
+#int
+entier = 2;
+#float
+float= 3.14;
+#string
+chaine = "Bonjour";
+#Boolean
+vrai = True;
+faux = False;
+#Complex
+complex_num = 3 +4j;
+#List
+liste = [1,2,3,4,5,6,7,8]
+#Tuple
+tuple = (1, 2, 3, 4, 5, 6, 7)
+#Set
+ensemble = {1,1,2,2,3,3,}
+#Dictionary
+dic = {"a":1, "b":2, "c":4}
