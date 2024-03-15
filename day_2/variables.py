0f78bc4990958daa21be7dbeb346f2723564c3d8
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-03-15 20:09:31 +0100

    :art: day2 level 1  :art:

---
+++ day_2/variables.py
@@ -0,0 +1,9 @@
+#Day 2: 30 Days of python programming
+
+#level 1
+first_name = "John";
+last_name= "Elton";
+full_name= first_name + " " + last_name;
+country= "US";
+city= "San Francisco";
+age=20;
