8cef140b963575ec703fb8ea632b68ad15681fa3
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-03-16 19:06:16 +0100

    :art:  day3 question 8 et 9  :art:

--- day_3/operators.py
+++ day_3/operators.py
@@ -16,6 +16,7 @@ height = int(input("Entrer height : "))
 area =  int(0.5 * base * height);
 print("The area of the triangle is  ", area) """
 
+
 #5
 """ side_a = int(input("Enter side a : "));
 side_b = int(input("Enter side b : "));
@@ -23,22 +24,35 @@ side_c = int(input("Enter side c : "));
 perim = (side_a+side_b+side_c)
 print("The perimeter of the triangle is" , perim); """
 
+
 #6
-length = int(input("Enter length : "));
+""" length = int(input("Enter length : "));
 width = int(input("Enter width : "));
 area = length * width;
 perimeter = 2 * (length + width)
-print("The area and the perimeter of the rectangle is ", area, perimeter);
+print("The area and the perimeter of the rectangle is ", area, perimeter); """
+
 
 #7
-radius = int(input("Enter radius : "));
+""" radius = int(input("Enter radius : "));
 area = 3.14 * radius * radius;
 circumference=  2 * 3.14 * radius;
-print("the area and the circumference of the circle is : ", area, "," ,circumference);
+print("the area and the circumference of the circle is : ", area, "," ,circumference); """
+
 
 #8
+print("x-intercept", 1)
+print("y-intercept", -2)
+print("slope", 2)
 
 #9
+import math
+x1, x2, y1, y2 = 2, 6, 2, 10;
+slope = ((y2 - y1)/(x2-x1))
+print("Slope : ", slope)
+distance = math.sqrt((x1 - x2)**2+ (y1 - y2)**2);
+print("Distance : ", distance);
+
 
 #10
 
