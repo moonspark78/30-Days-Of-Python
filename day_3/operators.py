4d793e6653e72b399f8ea667f931caec7bcd1345
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-03-16 18:14:58 +0100

    :art:  day3 question 6 et 7  :art:

--- day_3/operators.py
+++ day_3/operators.py
@@ -11,21 +11,30 @@ complix = 2 +4j;
 c_number = complex(3,4);
 
 #4
-#base = int(input("Entrer base : "))
-#height = int(input("Entrer height : "))
-#area =  int(0.5 * base * height);
-#print("The area of the triangle is  ", area)
+""" base = int(input("Entrer base : "))
+height = int(input("Entrer height : "))
+area =  int(0.5 * base * height);
+print("The area of the triangle is  ", area) """
 
 #5
-side_a = int(input("Enter side a : "));
+""" side_a = int(input("Enter side a : "));
 side_b = int(input("Enter side b : "));
 side_c = int(input("Enter side c : "));
 perim = (side_a+side_b+side_c)
-print("The perimeter of the triangle is" , perim);
+print("The perimeter of the triangle is" , perim); """
 
 #6
+length = int(input("Enter length : "));
+width = int(input("Enter width : "));
+area = length * width;
+perimeter = 2 * (length + width)
+print("The area and the perimeter of the rectangle is ", area, perimeter);
 
 #7
+radius = int(input("Enter radius : "));
+area = 3.14 * radius * radius;
+circumference=  2 * 3.14 * radius;
+print("the area and the circumference of the circle is : ", area, "," ,circumference);
 
 #8
 
