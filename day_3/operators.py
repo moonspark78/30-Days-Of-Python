27d3ea8c51aada0f47020e8e3c6d479c2738bff9
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-03-16 20:14:32 +0100

    :art:  day3 question 10 to  14  :art:

--- day_3/operators.py
+++ day_3/operators.py
@@ -41,29 +41,45 @@ print("the area and the circumference of the circle is : ", area, "," ,circumfer
 
 
 #8
-print("x-intercept", 1)
+""" print("x-intercept", 1)
 print("y-intercept", -2)
-print("slope", 2)
+print("slope", 2) """
 
 #9
-import math
+""" import math
 x1, x2, y1, y2 = 2, 6, 2, 10;
 slope = ((y2 - y1)/(x2-x1))
 print("Slope : ", slope)
 distance = math.sqrt((x1 - x2)**2+ (y1 - y2)**2);
-print("Distance : ", distance);
+print("Distance : ", distance); """
 
 
 #10
+#resultat_si_vrai if (condition) else (resultat_si_faux)
+x1, x2, y1, y2 = 2, 6, 2, 10;
+print(2 if 2 < (y2 - y1)/(x2-x1) else (y2 - y1)/(x2-x1))
+
 
 #11
+for x in range(0,9):
+    print(x **2 +6 * x + 9)
 
 #12
+print(len("python"))
+print(len("dragon"))
+print(len("python") > len("dragon"))
+print(not(len("python") == len("dragon")))
 
 #13
+print("on" in "dragon" and "on" in "python" )
 
 #14
+print("jargon" in "I hope this course is not full of jargon.")
 
 #15
 
 #16
+#17
+#18
+#19
+#20
