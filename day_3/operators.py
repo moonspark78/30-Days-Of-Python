599cde834ef846d7125a0491fd4bfaa61079d8a6
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-03-16 17:18:45 +0100

    :art:  day3  :art:

---
+++ day_3/operators.py
@@ -0,0 +1,41 @@
+#Operators
+
+#1
+age = 23;
+
+#2
+height=float(178);
+
+#3
+complix = 2 +4j;
+c_number = complex(3,4);
+
+#4
+base = int(input("Entrer base : "))
+height = int(input("Entrer height : "))
+area =  int(0.5 * base * height);
+print("The area of the triangle is  ", area)
+
+#5
+
+#6
+
+#7
+
+#8
+
+#9
+
+#10
+
+#11
+
+#12
+
+#13
+
+#14
+
+#15
+
+#16
